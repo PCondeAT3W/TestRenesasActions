ca93dfdcf12ed651da350d43f6a28a60638a3213
Pablo Conde <pconde@at3w.com>
2026-07-20 12:05:09 +0200

    fix cmake file

--- cmake/arm-none-eabi.cmake
+++ cmake/arm-none-eabi.cmake
@@ -5,16 +5,22 @@ set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
 set(ARM_GNU_TOOLCHAIN_BIN "" CACHE PATH "ARM GNU toolchain bin path")
 
-set(CMAKE_C_COMPILER   "${ARM_GNU_TOOLCHAIN_BIN}/arm-none-eabi-gcc.exe")
-set(CMAKE_CXX_COMPILER "${ARM_GNU_TOOLCHAIN_BIN}/arm-none-eabi-g++.exe")
-set(CMAKE_ASM_COMPILER "${ARM_GNU_TOOLCHAIN_BIN}/arm-none-eabi-gcc.exe")
+if(CMAKE_HOST_WIN32) 
+    set(TOOLCHAIN_EXECUTABLE_SUFFIX ".exe") 
+else() 
+    set(TOOLCHAIN_EXECUTABLE_SUFFIX "") 
+endif()
 
-set(CMAKE_AR     "${ARM_GNU_TOOLCHAIN_BIN}/arm-none-eabi-ar.exe")
-set(CMAKE_RANLIB "${ARM_GNU_TOOLCHAIN_BIN}/arm-none-eabi-ranlib.exe")
-set(CMAKE_NM     "${ARM_GNU_TOOLCHAIN_BIN}/arm-none-eabi-nm.exe")
+set(CMAKE_C_COMPILER   "${ARM_GNU_TOOLCHAIN_BIN}/arm-none-eabi-gcc${TOOLCHAIN_EXECUTABLE_SUFFIX}")
+set(CMAKE_CXX_COMPILER "${ARM_GNU_TOOLCHAIN_BIN}/arm-none-eabi-g++${TOOLCHAIN_EXECUTABLE_SUFFIX}")
+set(CMAKE_ASM_COMPILER "${ARM_GNU_TOOLCHAIN_BIN}/arm-none-eabi-gcc${TOOLCHAIN_EXECUTABLE_SUFFIX}")
 
-set(CMAKE_OBJCOPY "${ARM_GNU_TOOLCHAIN_BIN}/arm-none-eabi-objcopy.exe")
-set(CMAKE_SIZE    "${ARM_GNU_TOOLCHAIN_BIN}/arm-none-eabi-size.exe")
+set(CMAKE_AR     "${ARM_GNU_TOOLCHAIN_BIN}/arm-none-eabi-ar${TOOLCHAIN_EXECUTABLE_SUFFIX}")
+set(CMAKE_RANLIB "${ARM_GNU_TOOLCHAIN_BIN}/arm-none-eabi-ranlib${TOOLCHAIN_EXECUTABLE_SUFFIX}")
+set(CMAKE_NM     "${ARM_GNU_TOOLCHAIN_BIN}/arm-none-eabi-nm${TOOLCHAIN_EXECUTABLE_SUFFIX}")
+
+set(CMAKE_OBJCOPY "${ARM_GNU_TOOLCHAIN_BIN}/arm-none-eabi-objcopy${TOOLCHAIN_EXECUTABLE_SUFFIX}")
+set(CMAKE_SIZE    "${ARM_GNU_TOOLCHAIN_BIN}/arm-none-eabi-size${TOOLCHAIN_EXECUTABLE_SUFFIX}")
 
 # Evitar que CMake encuentre librerías/headers del host por accidente
 set(CMAKE_FIND_ROOT_PATH "${ARM_GNU_TOOLCHAIN_BIN}/..")
